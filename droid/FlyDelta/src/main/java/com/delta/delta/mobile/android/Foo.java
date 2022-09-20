426544b85817bcc05c8f09c518cb9e2ec0859fa7
dannybduval <daniel.b.duval@delta.com>
2022-09-20 14:09:12 -0400

    add code to see what happens

---
+++ droid/FlyDelta/src/main/java/com/delta/delta/mobile/android/Foo.java
@@ -0,0 +1,16 @@
+package com.delta.mobile.android;
+
+public class Foo {
+
+  private String name;
+
+  public Foo(String namee) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+}
+
